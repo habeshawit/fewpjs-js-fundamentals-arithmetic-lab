26a61047e4d57c6004d303e7da3245dcee4a5314
habeshawit <shalomseleshi@gmail.com>
2021-03-26 10:43:50 -0400

    commit tree 2ae14c2a1478a3ac9d572f377dbdc1e52a32b29d

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 60
+let num2 = 2
+let num3 = 14
+let num4 = 10
+let multiply = num1 + num2
+
+let random = Math.floor(Math.random() > 0)
+
+let mod = num3 % num4
+
+let max = Math.max(1,2,20,5)
